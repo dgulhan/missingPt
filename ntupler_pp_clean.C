5fa201dc93ff89546838dfb440e65401bb2d24c8
Doga Can Gulhan <dgulhan@lxplus407.cern.ch>
2014-03-22 03:30:32 +0100

    ntuplers cleaned up

--- ntupler_pp_clean.C
+++ ntupler_pp_clean.C
@@ -69,12 +69,10 @@ Float_t getAvePhi(Float_t inLeadPhi, Float_t inSubLeadPhi)
 }
 
 
-void ntupler_PYTHIA_clean(double ptmin_trk=8,double ptmax_trk=300){ 
+void ntupler_pp_clean(double ptmin_trk=8,double ptmax_trk=300){ 
 TH1D::SetDefaultSumw2();
  TString algo="ak3Calo"; 
- bool doGenJet=0;
  bool doSaveTrackInfo=false;
- bool doTrackTreeParts=false;
 
  //input file  
  cout<<"ptmin= "<<ptmin_trk<<" ptmax= "<<ptmax_trk<<endl;
@@ -131,42 +129,26 @@ TH1D::SetDefaultSumw2();
  }
  
  //output file and tree
- TFile *outf= new TFile(Form("ntuples_PYTHIA_mptonly_20140321/full_ntuple_%s_pt%d_%d_%s_doGenJet%d.root",infname.Data(),(int)ptmin_trk,(int)ptmax_trk,algo.Data(),doGenJet),"recreate");
- std::string partVars="pt:eta:phi:rmin:pNRec:smeared_pt:matchedpt:eff:trackselect:pt1:eta1:phi1:pt2:eta2:phi2:pt3:eta3:phi3:dphi:ptratio:jetspz:jetspt:jetseta:jetsrap:jetspz_12:jetspt_12:jetsrap_12:alpha:mpt_track:vz";
+ TFile *outf= new TFile(Form("ntuples_pp_mptonly_20140322/full_ntuple_%s_pt%d_%d_%s.root",infname.Data(),(int)ptmin_trk,(int)ptmax_trk,algo.Data()),"recreate");
  std::string trackVars="trackselect:eff:fake:trkfake:trkstatus:weight_unfold:pt:eta:phi:rmin:pt1:eta1:phi1:pt2:eta2:phi2:pt3:eta3:phi3:dphi:ptratio:jetspz:jetspt:jetseta:jetsrap:jetspz_12:jetspt_12:jetsrap_12:alpha:mpt_track:vz";
  
  
  TNtuple *nt_track = new TNtuple("nt_track","",trackVars.data());
- TNtuple *nt_particle = new TNtuple("nt_particle","",partVars.data());
- TNtuple *nt_gen = new TNtuple("nt_gen","","pt:eta:phi:vz:sta");
  
  
- std:string jetVars="pt1:eta1:phi1:pt2:eta2:phi2:pt3:eta3:phi3:dphi:ptratio:jetspz:jetspt:jetseta:jetsrap:jetsphi:jetspz_12:jetspt_12:jetseta_12:jetsrap_12:jetsphi_12:trkrap:mpt:mpt_s:mpt_b:mpt_tracks:mpt_tracks_ls:mpt_parts:mpt_tracks_uncorr:mpt_ave:mpt_b_ave:mpt_s_ave:mpt_tracks_ave:mpt_tracks_uncorr_ave:hfp:hfm:Npassingtrk:vz";
- 
- 
- 
+ std:string jetVars="pt1:eta1:phi1:pt2:eta2:phi2:pt3:eta3:phi3:dphi:ptratio:jetspz:jetspt:jetseta:jetsrap:jetsphi:jetspz_12:jetspt_12:jetseta_12:jetsrap_12:jetsphi_12:trkrap:mpt_tracks:mpt_tracks_ls:mpt_tracks_uncorr:mpt_tracks_ave:mpt_tracks_uncorr_ave:hfp:hfm:Npassingtrk:vz";
+  
  TNtuple *nt_jet = new TNtuple("nt_jet","",jetVars.data());
 
  std::string mptVars="alpha_0:alpha_1:alpha_2:alpha_3:alpha_4:alpha_5:alpha_6:alpha_7:alpha_8:alpha_9:alpha_10:alpha_11:alpha_12:alpha_13:alpha_14:alpha_15:alpha_16:alpha_17:alpha_18:alpha_19:alpha_20:alpha_21:alpha_22:alpha_23:alpha_24:alpha_25:alpha_26:alpha_27:alpha_28:alpha_29";
  
- TNtuple *nt_mpt=new TNtuple("nt_mpt","",mptVars.data()); 
- TNtuple *nt_mpt_b=new TNtuple("nt_mpt_b","",mptVars.data());
- TNtuple *nt_mpt_s=new TNtuple("nt_mpt_s","",mptVars.data());
- TNtuple *nt_mpt_parts=new TNtuple("nt_mpt_parts","",mptVars.data());
  TNtuple *nt_mpt_tracks=new TNtuple("nt_mpt_tracks","",mptVars.data());
  TNtuple *nt_mpt_tracks_uncorr=new TNtuple("nt_mpt_tracks_uncorr","",mptVars.data());
  
  std::string mptVars_dR="dR_0:dR_1:dR_2:dR_3:dR_4:dR_5:dR_6:dR_7:dR_8:dR_9:dR_10:dR_11:dR_12:dR_13:dR_14:dR_15:dR_16:dR_17:dR_18:dR_19:dR_20:dR_21:dR_22:dR_23:dR_24:dR_25:dR_26:dR_27:dR_28:dR_29:dR_30:dR_31:dR_32:dR_33:dR_34:dR_35";
  
- TNtuple *nt_mpt_parts_dR=new TNtuple("nt_mpt_parts_dR","",mptVars_dR.data());
  TNtuple *nt_mpt_tracks_dR=new TNtuple("nt_mpt_tracks_dR","",mptVars_dR.data());
  TNtuple *nt_mpt_tracks_uncorr_dR=new TNtuple("nt_mpt_tracks_uncorr_dR","",mptVars_dR.data());
- TNtuple *nt_mpt_s_dR=new TNtuple("nt_mpt_s_dR","",mptVars_dR.data());
- TNtuple *nt_mpt_b_dR=new TNtuple("nt_mpt_b_dR","",mptVars_dR.data());
- TNtuple *nt_mpt_dR=new TNtuple("nt_mpt_dR","",mptVars_dR.data());
- TNtuple *nt_mpt_s_dR_ave=new TNtuple("nt_mpt_s_dR_ave","",mptVars_dR.data());
- TNtuple *nt_mpt_b_dR_ave=new TNtuple("nt_mpt_b_dR_ave","",mptVars_dR.data());
- TNtuple *nt_mpt_dR_ave=new TNtuple("nt_mpt_dR_ave","",mptVars_dR.data());
  TNtuple *nt_mpt_tracks_dR_ave=new TNtuple("nt_mpt_tracks_dR_ave","",mptVars_dR.data());
  TNtuple *nt_mpt_tracks_uncorr_dR_ave=new TNtuple("nt_mpt_tracks_uncorr_dR_ave","",mptVars_dR.data());
 
@@ -185,7 +167,7 @@ TH1D::SetDefaultSumw2();
   
   // cout<<"pcoll "<<fskim->pcollisionEventSelection<<endl;
   // cout<<"noisefilter "<<fskim->pHBHENoiseFilter<<endl;
-    if(!(fskim->pPAcollisionEventSelectionPA))continue;
+  if(!(fskim->pPAcollisionEventSelectionPA && fskim->pHBHENoiseFilter))continue;
   
   // cout<<"passed selection"<<endl;
   float vz = fhi->vz;
@@ -194,27 +176,18 @@ TH1D::SetDefaultSumw2();
   float pt1=-99;
   float phi1=-99;
   float eta1=-99;
-  float refpt1=-99;
-  float refeta1=-99;
-  float refphi1=-99;
   float neutralMax1=-99;
   float trackMax1=-99;
   float trackN1=-99;
   float pt2=-99;
   float phi2=-99;
   float eta2=-99;
-  float refpt2=-99;
-  float refphi2=-99;
-  float refeta2=-99;
   float neutralMax2=-99;
   float trackMax2=-99;
   float trackN2=-99;
   float pt3=-99;
   float phi3=-99;
   float eta3=-99;
-  float refpt3=-99;
-  float refeta3=-99;
-  float refphi3=-99;
   float neutralMax3=-99;
   float trackMax3=-99;
   float trackN3=-99; 
@@ -224,27 +197,16 @@ TH1D::SetDefaultSumw2();
   
   int njet=0;
   TLorentzVector vjets;
-  if(!doGenJet){
-   for(int ijet=0;ijet<fjet->nref;ijet++){
-    if(fabs(fjet->jteta[ijet])>2 ||fjet->jtpt[ijet]<30) continue;
-    jets.push_back(std::make_pair(fjet->jtpt[ijet],std::make_pair(fjet->jteta[ijet],fjet->jtphi[ijet])));
-  
-    TLorentzVector vjet;
-    vjet.SetPtEtaPhiM(fjet->jtpt[ijet],fjet->jteta[ijet],fjet->jtphi[ijet],0);
-    vjets+=vjet;
-    njet++;
-   } 
-  }else{
-   for(int ijet=0;ijet<fjet->ngen;ijet++){
-    if(fabs(fjet->geneta[ijet])>2 ||fjet->genpt[ijet]<30) continue;
-    jets.push_back(std::make_pair(fjet->genpt[ijet],std::make_pair(fjet->geneta[ijet],fjet->genphi[ijet])));
-  
-    TLorentzVector vjet;
-    vjet.SetPtEtaPhiM(fjet->genpt[ijet],fjet->geneta[ijet],fjet->genphi[ijet],0);
-    vjets+=vjet;
-    njet++;
-   } 
-  }
+  for(int ijet=0;ijet<fjet->nref;ijet++){
+   if(fabs(fjet->jteta[ijet])>2 ||fjet->jtpt[ijet]<30) continue;
+   jets.push_back(std::make_pair(fjet->jtpt[ijet],std::make_pair(fjet->jteta[ijet],fjet->jtphi[ijet])));
+
+   TLorentzVector vjet;
+   vjet.SetPtEtaPhiM(fjet->jtpt[ijet],fjet->jteta[ijet],fjet->jtphi[ijet],0);
+   vjets+=vjet;
+   njet++;
+  } 
+
   double jetspx=-99;
   double jetspy=-99; 
   double jetspz=-99;
@@ -307,142 +269,34 @@ TH1D::SetDefaultSumw2();
   }
   
   
-  float mpt_ave=0;
   float mpt_tracks_ave=0;
   float mpt_tracks_uncorr_ave=0;
-  float mpt_s_ave=0;
-  float mpt_b_ave=0;
-
   float mpt_dR_tracks_uncorr_ave[35];
-  float mpt_dR_ave[35];
-  float mpt_dR_s_ave[35];
-  float mpt_dR_b_ave[35];
   float mpt_dR_tracks_ave[35];
-
-  float mpt=0;
-  float mpt_parts=0;
   float mpt_tracks=0;
   float mpt_tracks_ls=0;
   float mpt_tracks_uncorr=0;
-  float mpt_s=0;
-  float mpt_b=0;
-  float mpt_alpha_s[30];
-  float mpt_alpha_b[30];
-  float mpt_alpha[30];
   float mpt_alpha_tracks[30];  
-  float mpt_alpha_parts[30];
   float mpt_alpha_tracks_uncorr[30];
-  float mpt_dR_parts[35];
   float mpt_dR_tracks[35];
   float mpt_dR_tracks_uncorr[35];
-  float mpt_dR_s[35];
-  float mpt_dR_b[35];
-  float mpt_dR[35];
   
   
   for(int i=0;i<30;i++){
-   mpt_alpha[i]=0;  
-   mpt_alpha_s[i]=0;  
-   mpt_alpha_b[i]=0;  
    mpt_alpha_tracks[i]=0;  
    mpt_alpha_tracks_uncorr[i]=0; 
-   mpt_alpha_parts[i]=0;  
   }
   for(int i=0;i<35;i++){
    mpt_dR_tracks[i]=0;  
-   mpt_dR_tracks_uncorr[i]=0;  
-   mpt_dR_parts[i]=0;  
-   mpt_dR[i]=0;  
-   mpt_dR_s[i]=0;  
-   mpt_dR_b[i]=0; 
+   mpt_dR_tracks_uncorr[i]=0; 
       
    mpt_dR_tracks_ave[i]=0;  
    mpt_dR_tracks_uncorr_ave[i]=0;  
-   mpt_dR_s_ave[i]=0;  
-   mpt_dR_b_ave[i]=0;  
-   mpt_dR_ave[i]=0;  
   }
   
-  TLorentzVector vparticles, vsignal, vbackground,vtracks;
+  TLorentzVector vtracks;
 
-  if(doTrackTreeParts){
-   for(int itrk=0;itrk<ftrk->nParticle;itrk++){
   
-   float eta=ftrk->pEta[itrk];
-   float pt=ftrk->pPt[itrk];
-   float matchedpt=ftrk->mtrkPt[itrk];
-   float phi=ftrk->pPhi[itrk];
-   float pNRec =ftrk->pNRec[itrk];
-   float smeared_pt=pt*(1+h_res->GetBinContent(h_res->FindBin(pt))*fgaus->GetRandom());
-
-   float eff_pt,eff_accept,eff_rmin;
-   eff_pt=eff_accept=eff_rmin=1;
-   float fake_pt,fake_accept,fake_rmin;
-   fake_pt=fake_accept=fake_rmin=0;
-   
-   if(fabs(eta)>2.4) continue;
-   if(pt<=ptmin_trk || pt>ptmax_trk) continue; 
-   float trackselect=(ftrk->mtrkQual[itrk] && fabs(ftrk->mtrkDxy1[itrk]/ftrk->mtrkDxyError1[itrk])<3.0 && fabs(ftrk->mtrkDz1[itrk]/ftrk->mtrkDzError1[itrk])<3 && (ftrk->mtrkPtError[itrk]/ftrk->mtrkPt[itrk])<0.1);
-   
-   float weight_unfold=hweight->GetBinContent(hweight->FindBin(pt));
-   
-   float rmin=100;
- 
-   //find rmin; 
-   for(int ijet=0;ijet<fjet->nref;ijet++){
-    if(fabs(fjet->jteta[ijet])>2 || fjet->jtpt[ijet]<30) continue;
-    float r_reco=sqrt(pow(eta-fjet->jteta[ijet],2)+pow(acos(cos(phi-fjet->jtphi[ijet])),2));
-    if(r_reco<rmin)rmin=r_reco;
-   }
-   
-   for(int ipt=0;ipt<npt;ipt++){
-    if(pt>=ptmin[ipt] && pt<ptmax[ipt]){
-      eff_pt=p_eff_pt[ipt]->GetBinContent(p_eff_pt[ipt]->FindBin(pt));
-      eff_accept=p_eff_accept[ipt]->GetBinContent(p_eff_accept[ipt]->GetXaxis()->FindBin(phi),p_eff_accept[ipt]->GetYaxis()->FindBin(eta));
-      if(rmin<5)eff_rmin=p_eff_rmin[ipt]->GetBinContent(p_eff_rmin[ipt]->FindBin(rmin));
-     }     
-   }   
-    
-   float eff=1;
-   eff=eff_accept*eff_pt*eff_rmin;
-   if(eff==0){
-   if(pt>100)eff=0.8;
-	  else eff=1;
-   }
-   
-   TLorentzVector vp;
-   vp.SetPtEtaPhiM(pt,eta,phi,0.13957018);
-   
-   float alpha=-99;
-   float alpha_sub=-99;
-    
-   float mpt_track=pt*cos(phi-phi1);
-   mpt_parts+=pt*cos(phi-phi1);
-   alpha=vp.Angle(v1.Vect());
-   
-   float entry[]={pt,eta,phi,rmin,pNRec,smeared_pt,matchedpt,eff,trackselect,pt1,eta1,phi1,pt2,eta2,phi2,pt3,eta3,phi3,dphi,ptratio,jetspz,jetspt,jetseta,jetsrap,jetspz_12,jetspt_12,jetsrap_12,alpha,mpt_track,vz};
-   nt_particle->Fill(entry);
-   
-   if(njet>1){
-	
-    double mpt_alpha_t[30];
-    double mpt_dR_t[35];
-    
-   for(int i=0;i<30;i++){
-      mpt_alpha_t[i]=0;
-	  if(alpha <(frac[i+1]*TMath::Pi()) && alpha >=(frac[i]*TMath::Pi())) mpt_alpha_t[i] =pt*cos(phi -phi1);
-   }
-    for(int i=0;i<35;i++){
-      mpt_dR_t[i]=0;
-      double dR1=sqrt(pow(acos(cos(phi1-phi)),2)+pow(eta1-eta,2));
-	  double dR2=sqrt(pow(acos(cos(phi2-phi)),2)+pow(eta2-eta,2));
-      if(((dR1 < dR_upperbound[i+1]) && dR1 >=dR_upperbound[i]) || ((dR2 < dR_upperbound[i+1]) && dR2 >=dR_upperbound[i])) mpt_dR_t[i] =pt*cos(phi -phi1);
-      mpt_dR_parts[i]+=mpt_dR_t[i];
-	}
-   } 
-
-  }
-  }
   int Npassingtrk=0;
   for(int itrk=0;itrk<ftrk->nTrk;itrk++){
    float eta=ftrk->trkEta[itrk];
@@ -553,133 +407,16 @@ TH1D::SetDefaultSumw2();
 
   }
   
-  for(int itrk=0;itrk<fgen->mult;itrk++){
-   
-   float eta=fgen->eta[itrk];
-   if(fabs(eta)>2.4) continue; //acceptance of the tracker
-   float pt=fgen->pt[itrk];
-   float matchedpt=pt;
-   if(pt<ptmin_trk || pt>ptmax_trk) continue; 
-   if(fgen->chg[itrk]==0)continue;   
-   float phi=fgen->phi[itrk];
-   float sta=fgen->sta[itrk];
-   
-   float trackselect=1;
-   
-   float rmin=100;
-   
-   float entry[]={pt,eta,phi,vz,sta};
-   if(doSaveTrackInfo) nt_gen->Fill(entry);
-   
-   for(int ijet=0;ijet<fjet->nref;ijet++){
-    if(fabs(fjet->jteta[ijet])>2 || fjet->jtpt[ijet]<30) continue;
-    float r_reco=sqrt(pow(eta-fjet->jteta[ijet],2)+pow(acos(cos(phi-fjet->jtphi[ijet])),2));
-    if(r_reco<rmin)rmin=r_reco;
-   }
-   
-   TLorentzVector vp;
-   vp.SetPtEtaPhiM(pt,eta,phi,0.13957018);
-   vparticles+=vp;
-   float signal=0;
-   if(fgen->sube[itrk]==0)signal=1;
-   if(signal==1)vsignal+=vp;
-   if(signal==0)vbackground+=vp;
-   
-   float alpha=-99;
-   float  mpt_track_s_ave=0;
-   float  mpt_track_b_ave=0;
-   float  mpt_track_s=0;
-   float  mpt_track_b=0;
-    
-	 float mpt_track=pt*cos(phi-phi1);
-	 float mpt_track_ave=pt*cos(phi-phiave);
-   if(signal==1){ 
-	  mpt_track_s=pt*cos(phi-phi1);
-	  mpt_track_s_ave=mpt_track_ave;
-   }
-   else{ 
-    mpt_track_b=pt*cos(phi-phi1);
-    mpt_track_b_ave=mpt_track_ave;
-   }
-   mpt+=mpt_track;
-   mpt_ave+=mpt_track_ave;
-   mpt_s+=mpt_track_s;
-   mpt_s_ave+=mpt_track_s_ave;
-   mpt_b+=mpt_track_b;
-   mpt_b_ave+=mpt_track_b_ave;
-   
-   alpha=vp.Angle(v1.Vect());
-
-   if(njet>1){
-	
-	
-    double mpt_alpha_t[30];
-    double mpt_dR_t[35];
-    double mpt_dR_t_ave[35];
-        
-   for(int i=0;i<30;i++){
-     mpt_alpha_t[i]=0;
-	   if(alpha <(frac[i+1]*TMath::Pi()) && alpha >=(frac[i]*TMath::Pi())) mpt_alpha_t[i] =pt*cos(phi -phi1);
-     mpt_alpha[i]+=mpt_alpha_t[i];
-     if(signal==1) mpt_alpha_s[i]+=mpt_alpha_t[i];
-     else mpt_alpha_b[i]+=mpt_alpha_t[i];
-    }     
-     
-    for(int i=0;i<35;i++){
-    
-      mpt_dR_t[i]=0;
-      mpt_dR_t_ave[i]=0;
-	    double dR1=sqrt(pow(acos(cos(phi1-phi)),2)+pow(eta1-eta,2));
-	    double dR2=sqrt(pow(acos(cos(phi2-phi)),2)+pow(eta2-eta,2));
-	    if(((dR1 < dR_upperbound[i+1]) && dR1 >=dR_upperbound[i]) || ((dR2 <   dR_upperbound[i+1]) && dR2 >=dR_upperbound[i])){
-  	   mpt_dR_t[i] =pt*cos(phi -phi1);
-	     mpt_dR_t_ave[i]=pt*cos(phi-phiave);
-	    }
-      mpt_dR[i]+=mpt_dR_t[i];
-      mpt_dR_ave[i]+=mpt_dR_t_ave[i];
-      if(signal==1){
-	     mpt_dR_s[i]+=mpt_dR_t[i];
-	     mpt_dR_s_ave[i]+=mpt_dR_t_ave[i];
-	    }else{ 
- 	     mpt_dR_b[i]+=mpt_dR_t[i];
-  	   mpt_dR_b_ave[i]+=mpt_dR_t_ave[i];
-	  }	  
-	} 
-   }
-   
-   
-   //fill in the output tree
-   // float pNRec =ftrk->pNRec[itrk];
-   float pNRec =1;
-
-  }
-   
-  double partrap=vparticles.Rapidity();
   double trkrap=vtracks.Rapidity();
-  double signalrap=vsignal.Rapidity();
-  double backgroundrap=vbackground.Rapidity();
   
-  
-  nt_mpt->Fill(mpt_alpha);
-  nt_mpt_s->Fill(mpt_alpha_s);
-  nt_mpt_b->Fill(mpt_alpha_b);
-  nt_mpt_parts->Fill(mpt_alpha_parts);
   nt_mpt_tracks->Fill(mpt_alpha_tracks);
   nt_mpt_tracks_uncorr->Fill(mpt_alpha_tracks_uncorr);
-
-  nt_mpt_dR->Fill(mpt_dR);
-  nt_mpt_s_dR->Fill(mpt_dR_s);
-  nt_mpt_b_dR->Fill(mpt_dR_b);
-  nt_mpt_parts_dR->Fill(mpt_dR_parts);
   nt_mpt_tracks_dR->Fill(mpt_dR_tracks);
   nt_mpt_tracks_uncorr_dR->Fill(mpt_dR_tracks_uncorr);
-  nt_mpt_s_dR_ave->Fill(mpt_dR_s_ave);
-  nt_mpt_b_dR_ave->Fill(mpt_dR_b_ave);
-  nt_mpt_dR_ave->Fill(mpt_dR_ave);
   nt_mpt_tracks_dR_ave->Fill(mpt_dR_tracks_ave);
   nt_mpt_tracks_uncorr_dR_ave->Fill(mpt_dR_tracks_uncorr_ave);
   
-  float jtentry[]={pt1,eta1,phi1,pt2,eta2,phi2,pt3,eta3,phi3,dphi,ptratio,jetspz,jetspt,jetseta,jetsrap,jetsphi,jetspz_12,jetspt_12,jetseta_12,jetsrap_12,jetsphi_12,trkrap,mpt,mpt_s,mpt_b,mpt_tracks,mpt_tracks_ls,mpt_parts,mpt_tracks_uncorr,mpt_ave,mpt_b_ave,mpt_s_ave,mpt_tracks_ave,mpt_tracks_uncorr_ave,hfp,hfm,Npassingtrk,vz};
+  float jtentry[]={pt1,eta1,phi1,pt2,eta2,phi2,pt3,eta3,phi3,dphi,ptratio,jetspz,jetspt,jetseta,jetsrap,jetsphi,jetspz_12,jetspt_12,jetseta_12,jetsrap_12,jetsphi_12,trkrap,mpt_tracks,mpt_tracks_ls,mpt_tracks_uncorr,mpt_tracks_ave,mpt_tracks_uncorr_ave,hfp,hfm,Npassingtrk,vz};
   
  
   nt_jet->Fill(jtentry);
@@ -690,27 +427,13 @@ TH1D::SetDefaultSumw2();
  outf->cd();
  if(doSaveTrackInfo){
   nt_track->Write();
-  if(doTrackTreeParts) nt_particle->Write();
-  nt_gen->Write();
  }
  nt_jet->Write();
- 
-  nt_mpt->Write();
-  nt_mpt_s->Write();
-  nt_mpt_b->Write(); 
-  nt_mpt_dR->Write();
-  nt_mpt_s_dR->Write();
-  nt_mpt_b_dR->Write();
-  nt_mpt_parts->Write();  
-  nt_mpt_parts_dR->Write();
   
   nt_mpt_tracks->Write();
   nt_mpt_tracks_dR->Write();
   nt_mpt_tracks_uncorr->Write();
   nt_mpt_tracks_uncorr_dR->Write();
-  nt_mpt_s_dR_ave->Write();
-  nt_mpt_b_dR_ave->Write();
-  nt_mpt_dR_ave->Write();
   nt_mpt_tracks_dR_ave->Write();
   nt_mpt_tracks_uncorr_dR_ave->Write();
  outf->Close();
